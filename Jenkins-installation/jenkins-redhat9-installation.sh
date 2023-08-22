0780f82d95c15460849d87f3ba863f904c6756f7
nfestaclass02 <137131319+nfestaclass02@users.noreply.github.com>
2023-08-22 14:19:05 -0400

    Update jenkins-redhat9-installation.sh

--- Jenkins-installation/jenkins-redhat9-installation.sh
+++ Jenkins-installation/jenkins-redhat9-installation.sh
@@ -1,27 +1,21 @@
-#!/bin/bash
-# Author:  prof Festus N.
-# BU: NT/NSS
-# date: August 15 2023
-# Installing Jenkins on RHEL 7/8/9, CentOS 7/8/9 or Amazon Linux OS
+#!/bin/bashs
+# Author: Prof Festus
+# date: 08/21/2023
+# Installing Jenkins on RHEL 7/8, CentOS 7/8 or Amazon Linux OS
 # You can execute this script as user-data when launching your EC2 VM.
+sudo yum install git wget -y
 sudo timedatectl set-timezone America/New_York
 sudo hostnamectl set-hostname jenkins
-sudo yum install vim wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade -y
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
 # Add required dependencies for the jenkins package
-sudo yum install java-11-openjdk -y
-sudo yum install jenkins -y
+sudo yum install java-11-openjdk
+sudo yum install jenkins
 sudo systemctl daemon-reload
-# start jenkins
-# Start Jenkins
-# You can enable the Jenkins service to start at boot with the command:
 sudo systemctl enable jenkins
-#You can start the Jenkins service with the command:=
 sudo systemctl start jenkins
-# You can check the status of the Jenkins service using the command:
 sudo systemctl status jenkins
-sudo su - ec2-user
 echo "echo of jenkins installation"
+sudo su - ec2-user
